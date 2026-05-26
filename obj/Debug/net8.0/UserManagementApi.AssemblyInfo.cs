99116496614087ba657f29bb9415afeda80cd2b5
Marcelo Guimaraes da Silva <marceloguimaraes@outlook.com.br>
2026-05-25 23:17:15 -0300

    Add configuration files

--- obj/Debug/net8.0/UserManagementApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/UserManagementApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagementApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b2344ad57e543cfa7b6afb275e5810794289151")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagementApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagementApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
