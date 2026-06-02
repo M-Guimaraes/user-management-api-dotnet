7ade4a028976dda978edfcb551250c21a2015480
Marcelo Guimaraes da Silva <marceloguimaraes@outlook.com.br>
2026-06-02 20:03:44 -0300

    chore(migrations): rename refresh token column Token -> TokenHash

---
+++ Migrations/20260602225555_RenameRefreshTokenTokenToTokenHash.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserManagementApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameRefreshTokenTokenToTokenHash : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Token",
+                table: "refresh_tokens",
+                newName: "TokenHash");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TokenHash",
+                table: "refresh_tokens",
+                newName: "Token");
+        }
+    }
+}
